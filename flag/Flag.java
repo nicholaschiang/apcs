a8d8cef95f191332a4d3f85b3ea261d8a46cbddc
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-13 11:18:28 -0700

    wip(flag): add flag project starter code

---
+++ flag/Flag.java
@@ -0,0 +1,71 @@
+// Flag project started by Josh Paley, finished by Nicholas Chiang.
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JApplet;
+
+public class Flag extends JApplet {
+	private final int STRIPES = 13;
+
+	// SCALE FACTORS (A through L)
+	//
+	// Note: Constants in Java should always be ALL_CAPS, even
+	// if we are using single letters to represent them
+	//
+	// NOTE 2: Do not delete or change the names of any of the
+	// variables given here
+	//
+	// Set the constants to exactly what is specified in the documentation
+	// REMEMBER: These are scale factors.  They are not numbers of pixels.
+	// You will use these and the width and height of the Applet to figure
+	// out how to draw the parts of the flag (stripes, stars, field).
+	private final double A = 1.0;  // Hoist (width) of flag
+	private final double B = 1.9;  // Fly (length) of flag
+	private final double C = 0.5385;  // Hoist of Union
+	private final double D = 0.76;  // Fly of Union
+	private final double E = 0.054;  // See flag specification
+	private final double F = 0.054;  // See flag specification
+	private final double G = 0.063;  // See flag specification
+	private final double H = 0.063;  // See flag specification
+	private final double K = 0.0616;  // Diameter of star
+	private final double L = 0.0769;  // Width of stripe
+
+	// You will need to set values for these in paint()
+	private double flag_width;   // width of flag in pixels
+	private double flag_height;  // height of flag in pixels
+	private double stripe_height; // height of an individual stripe in pixels
+
+	// init() will automatically be called when an applet is run
+	public void init() {
+		// Choice of width = 1.9 * height to start off
+		// 760 : 400 is ratio of FLY : HOIST
+		setSize(760, 400);
+		repaint();
+	}
+
+	// paint() will be called every time a resizing of an applet occurs
+	public void paint(Graphics g) {
+		drawBackground(g);
+		drawStripes(g);
+		drawField(g);
+		drawStars(g);
+	}
+
+	private void drawBackground(Graphics g) {
+		g.fillRect(0, 0, getWidth(), getHeight());
+	}
+	
+	public void drawStripes(Graphics g) {
+		for (int idx = 0; idx < STRIPES; idx++) {
+			
+		}
+	}
+
+	public void drawField(Graphics g) {
+	}
+
+	public void drawStars(Graphics g) {
+	}
+}
