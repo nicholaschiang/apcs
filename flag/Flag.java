b4e02862a95bcd25f32f9f618daff4e05748f7c5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-08-18 11:18:27 -0700

    wip(flag): create star static methods

--- flag/Flag.java
+++ flag/Flag.java
@@ -3,6 +3,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.Path2D;
 
 import javax.swing.JApplet;
 
@@ -74,8 +76,38 @@ public class Flag extends JApplet {
 	}
 
 	public void drawStars(Graphics g) {
-		g.setColor(Color.WHITE);
-		for (int idx = 0; idx < STARS; idx++) {
+    g.setColor(Color.WHITE);
+		((Graphics2D) g).fill(createDefaultStar(50, 200, 200));
+	}
+	
+	private static Shape createDefaultStar(double radius, double centerX, double centerY) {
+		return createStar(centerX, centerY, radius, radius * 2.63, 5,
+				Math.toRadians(-18));
+	}
+
+	private static Shape createStar(double centerX, double centerY, double innerRadius, double outerRadius, int numRays, double startAngleRad) {
+		Path2D path = new Path2D.Double();
+		double deltaAngleRad = Math.PI / numRays;
+		for (int i = 0; i < numRays * 2; i++) {
+			double angleRad = startAngleRad + i * deltaAngleRad;
+			double ca = Math.cos(angleRad);
+			double sa = Math.sin(angleRad);
+			double relX = ca;
+			double relY = sa;
+			if ((i & 1) == 0) {
+				relX *= outerRadius;
+				relY *= outerRadius;
+			} else {
+				relX *= innerRadius;
+				relY *= innerRadius;
+			}
+			if (i == 0) {
+				path.moveTo(centerX + relX, centerY + relY);
+			} else {
+				path.lineTo(centerX + relX, centerY + relY);
+			}
 		}
+		path.closePath();
+		return path;
 	}
 }
